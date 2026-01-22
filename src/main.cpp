06f88388f14315cef9a792a9fc5b97e0d51723ee
Calum Kerr <calumxkerr@gmail.com>
2026-01-22 13:59:41 +0000

    Add security headers to the response for the /api/convert route

--- src/main.cpp
+++ src/main.cpp
@@ -46,6 +46,7 @@ int main() {
     buffer << file.rdbuf();
     auto page = crow::response(buffer.str());
     page.set_header("Content-Type", "text/html; charset=utf-8");
+    add_security_headers(page);
     return page;
   });
   CROW_ROUTE(app, "/app.js").methods("GET"_method)
