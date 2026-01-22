1e25bf033d2e1ad01fcb250155367de7cc064c24
Calum Kerr <calumxkerr@gmail.com>
2026-01-22 13:47:00 +0000

    Add service worker for caching and offline support

---
+++ frontend/service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'revisepdf-v1';
+const ASSETS = [
+  '/',
+  '/app',
+  '/index.html',
+  '/home.html',
+  '/app.js',
+  '/manifest.json'
+];
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then((response) => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      });
+    }).catch(() => {
+      return caches.match('/app');
+    })
+  );
+});
+
